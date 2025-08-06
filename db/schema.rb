31b1103760da655723b5874047554aa82ab06e5c
Vladislav Kostikov <vlad@kostikov.ru>
2025-08-06 15:05:25 +0300

    Add Task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_08_06_115043) do
+ActiveRecord::Schema.define(version: 2025_08_06_120152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "author_id"
+    t.integer "assignee_id"
+    t.string "state"
+    t.date "expired_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
