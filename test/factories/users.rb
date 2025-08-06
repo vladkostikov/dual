e1112109cf2411fd584c127a2daa2024a9b730d1
Vladislav Kostikov <vlad@kostikov.ru>
2025-08-06 15:40:54 +0300

    Add initial tests with factories

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,10 +1,22 @@
 FactoryBot.define do
   factory :user do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    password { "" }
-    email { "MyString" }
-    avatar { "MyString" }
+    first_name
+    last_name
+    password
+    email
+    avatar
     type { "" }
+
+    factory :admin do
+      type { "Admin" }
+    end
+
+    factory :developer do
+      type { "Developer" }
+    end
+
+    factory :manager do
+      type { "Manager" }
+    end
   end
 end
--- test/models/developer_test.rb
+++ test/models/developer_test.rb
@@ -1,4 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class DeveloperTest < ActiveSupport::TestCase
+  test "create" do
+    developer = create :developer
+    assert developer.persisted?
+  end
 end
