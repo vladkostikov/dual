1bd311247c5126b0dfa5b5c8b12a3dd25ca48390
Vladislav Kostikov <vlad@kostikov.ru>
2025-08-07 14:27:19 +0300

    Add relations and validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
+
+  validates :name, :description, :author, presence: true
+  validates :description, length: { maximum: 500 }
 end
