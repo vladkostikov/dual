8a589233d50c0ca6a10b8a13a6b3e744ad7ef4e8
Vladislav Kostikov <vlad@kostikov.ru>
2025-08-21 03:00:33 +0300

    Fix code style

--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -6,18 +6,18 @@ class Web::SessionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should post create" do
+  test 'should post create' do
     password = generate(:string)
     user = create(:user, { password: password })
     attrs = {
       email: user.email,
-      password: password
+      password: password,
     }
     post :create, params: { session_form: attrs }
     assert_response :redirect
   end
 
-  test "should delete destroy" do
+  test 'should delete destroy' do
     delete :destroy
     assert_response :redirect
   end
