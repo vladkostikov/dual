8844a7cd7beb463b68faa2135bffa428de88e884
Vladislav Kostikov <vlad@kostikov.ru>
2025-08-25 15:12:58 +0300

    Refactoring

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,4 @@
 class Admin::UsersController < Admin::ApplicationController
-
   def index
     @q = User.ransack(params[:q])
     @users = @q.result.order(:type).order(:id).page(params[:page]).per(params[:per_page])
@@ -20,25 +19,25 @@ class Admin::UsersController < Admin::ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_users_url
+      redirect_to(admin_users_url)
     else
-      render :new_admin_user_url
+      render(:new_admin_user_url)
     end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to admin_users_url
+      redirect_to(admin_users_url)
     else
-      render :edit
+      render(:edit)
     end
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to admin_users_path
+    redirect_to(admin_users_path)
   end
 
   private
