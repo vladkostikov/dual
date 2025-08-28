0c9088d08a4be8b4a3ea7b0656d9e0902fc48dd4
Vladislav Kostikov <vlad@kostikov.ru>
2025-08-28 04:17:51 +0300

    Fix code style

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ApplicationController < Api::ApplicationController
-  RANSACK_DEFAULT_SORT = 'id ASC'
+  RANSACK_DEFAULT_SORT = 'id ASC'.freeze
 
   def self.responder
     JsonResponder
@@ -11,7 +11,7 @@ class Api::V1::ApplicationController < Api::ApplicationController
       total_count: collection.total_count,
       current_page: collection.current_page,
       total_pages: collection.total_pages,
-      per_page: collection.limit_value
+      per_page: collection.limit_value,
     }
   end
 
