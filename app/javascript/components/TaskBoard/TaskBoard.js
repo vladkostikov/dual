e139adcfaed22bd4b41b686d5bdd4553d387357b
Vladislav Kostikov <vlad@kostikov.ru>
2025-09-18 08:19:06 +0300

    Add task creation with popup form

--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import KanbanBoard from '@asseinfo/react-kanban';
 import { propOr } from 'ramda';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 import Task from '../Task';
 import TasksRepository from '../../repositories/TasksRepository';
 import ColumnHeader from '../ColumnHeader';
+import TaskForm from '../../forms/TaskForm';
+import AddPopup from '../AddPopup';
+
+import useStyles from './useStyles';
 
 const STATES = [
   { key: 'new_task', value: 'New' },
@@ -15,6 +21,11 @@ const STATES = [
   { key: 'released', value: 'Released' },
   { key: 'archived', value: 'Archived' },
 ];
+
+const MODES = {
+  ADD: 'add',
+  NONE: 'none',
+};
 
 const initialBoard = {
   columns: STATES.map((column) => ({
@@ -27,7 +38,14 @@ const initialBoard = {
 
 function TaskBoard() {
   const [board, setBoard] = useState(initialBoard);
-  const [boardCards, setBoardCards] = useState([]);
+  const [boardCards, setBoardCards] = useState({});
+  const [mode, setMode] = useState(MODES.NONE);
+  const styles = useStyles();
+
+  // eslint-disable-next-line no-use-before-define
+  useEffect(() => loadBoard(), []);
+  // eslint-disable-next-line no-use-before-define
+  useEffect(() => generateBoard(), [boardCards]);
 
   const loadColumn = (state, page, perPage) =>
     TasksRepository.index({
@@ -49,7 +67,10 @@ function TaskBoard() {
     loadColumn(state, page, perPage).then(({ data }) => {
       setBoardCards((prevState) => ({
         ...prevState,
-        [state]: { cards: [...prevState[state].cards, ...data.items], meta: data.meta },
+        [state]: {
+          cards: [...(prevState[state]?.cards || []), ...(data?.items || [])],
+          meta: data?.meta || {},
+        },
       }));
     });
   };
@@ -59,7 +80,7 @@ function TaskBoard() {
       columns: STATES.map(({ key, value }) => ({
         id: key,
         title: value,
-        cards: propOr([], 'cards', boardCards[key]),
+        cards: propOr({}, 'cards', boardCards[key]),
         meta: propOr({}, 'meta', boardCards[key]),
       })),
     };
@@ -88,17 +109,36 @@ function TaskBoard() {
       });
   };
 
-  useEffect(() => loadBoard(), []);
-  useEffect(() => generateBoard(), [boardCards]);
+  const handleAddPopupOpen = () => {
+    setMode(MODES.ADD);
+  };
+
+  const handleClose = () => {
+    setMode(MODES.NONE);
+  };
+
+  const handleTaskCreate = (params) => {
+    const attributes = TaskForm.attributesToSubmit(params);
+    return TasksRepository.create({ task: attributes }).then(({ data }) => {
+      loadColumnInitial(data.task.state);
+      handleClose();
+    });
+  };
 
   return (
-    <KanbanBoard
-      renderCard={(card) => <Task task={card} />}
-      renderColumnHeader={(column) => <ColumnHeader column={column} onLoadMore={loadColumnMore} />}
-      onCardDragEnd={handleCardDragEnd}
-    >
-      {board}
-    </KanbanBoard>
+    <div>
+      <KanbanBoard
+        renderCard={(card) => <Task task={card} />}
+        renderColumnHeader={(column) => <ColumnHeader column={column} onLoadMore={loadColumnMore} />}
+        onCardDragEnd={handleCardDragEnd}
+      >
+        {board}
+      </KanbanBoard>
+      <Fab className={styles.addButton} onClick={handleAddPopupOpen} color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+      {mode === MODES.ADD && <AddPopup onCardCreate={handleTaskCreate} onClose={handleClose} />}
+    </div>
   );
 }
 
