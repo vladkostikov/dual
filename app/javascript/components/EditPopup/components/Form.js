e81598106e652e8690c7a8c4e7f0dda3b9c0e5e9
Vladislav Kostikov <vlad@kostikov.ru>
2025-09-19 15:57:14 +0300

    Integrate UserSelect component with Users API for author and assignee fields

--- app/javascript/components/EditPopup/components/Form.js
+++ app/javascript/components/EditPopup/components/Form.js
@@ -5,10 +5,13 @@ import { has } from 'ramda';
 import TextField from '@material-ui/core/TextField';
 
 import useStyles from './useStyles';
+import UserSelect from '../../../packs/components/UserSelect';
 
 function Form({ errors, onChange, task }) {
   const handleChangeTextField = (fieldName) => (event) => onChange({ ...task, [fieldName]: event.target.value });
   const styles = useStyles();
+
+  const handleChangeSelect = (fieldName) => (user) => onChange({ ...task, [fieldName]: user });
 
   return (
     <form className={styles.root}>
@@ -30,6 +33,23 @@ function Form({ errors, onChange, task }) {
         required
         multiline
         margin="dense"
+      />
+      <UserSelect
+        label="Author"
+        value={task.author}
+        onChange={handleChangeSelect('author')}
+        isDisabled
+        isRequired
+        error={has('author', errors)}
+        helperText={errors.author}
+      />
+      <UserSelect
+        label="Assignee"
+        value={task.assignee}
+        onChange={handleChangeSelect('assignee')}
+        isRequired
+        error={has('assignee', errors)}
+        helperText={errors.assignee}
       />
     </form>
   );
