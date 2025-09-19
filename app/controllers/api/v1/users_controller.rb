39587af7634044edfa6a8ec66154ee1f0e446d31
Vladislav Kostikov <vlad@kostikov.ru>
2025-09-19 15:26:22 +0300

    Add users API with index and show endpoints

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::UsersController < Api::V1::ApplicationController
+  respond_to :json
+
+  def show
+    user = User.find(params[:id])
+
+    respond_with(user, serializer: UserSerializer)
+  end
+
+  def index
+    users = User.ransack(ransack_params).result.page(page).per(per_page)
+
+    respond_with(users, each_serializer: UserSerializer, meta: build_meta(users), root: 'items')
+  end
+end
