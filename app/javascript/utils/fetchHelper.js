6420481ed4361aa3155656f6616f8802cb5e7629
Vladislav Kostikov <vlad@kostikov.ru>
2025-09-21 02:05:07 +0300

    Fix axios.delete

--- app/javascript/utils/fetchHelper.js
+++ app/javascript/utils/fetchHelper.js
@@ -56,8 +56,7 @@ export default {
     return axios.put(url, body).then(camelize);
   },
 
-  delete(url, json = {}) {
-    const body = decamelize(json);
-    return axios.delete(url, { data: body }).then(camelize);
+  delete(url) {
+    return axios.delete(url).then(camelize);
   },
 };
