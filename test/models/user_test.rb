36f478fdf0e0e3e76767dc42a93dd8b715866af5
Vladislav Kostikov <vlad@kostikov.ru>
2025-10-08 22:35:38 +0300

    Fix User email validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,4 +5,10 @@ class UserTest < ActiveSupport::TestCase
     user = create(:user)
     assert user.persisted?
   end
+
+  test 'email format validation' do
+    user = build(:user, email: '1@1')
+    assert_not user.valid?
+    assert_includes user.errors[:email], 'is invalid'
+  end
 end
