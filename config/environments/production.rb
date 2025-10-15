2fcd3877013349bd218c4296da6dafc82266726d
Vladislav Kostikov <vlad@kostikov.ru>
2025-10-15 07:00:38 +0300

    Configure production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['MAILER_USERNAME'],
+    password: ENV['MAILER_PASSWORD'],
+    address: ENV['MAILER_ADDRESS'],
+    port: ENV['MAILER_PORT'],
+    domain: ENV['MAILER_DOMAIN'],
+    authentication: ENV['MAILER_AUTHENTICATION'],
+    enable_starttls_auto: true,
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
