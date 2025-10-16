b488d65ab7b900064287a725709f80056b908edf
Vladislav Kostikov <vlad@kostikov.ru>
2025-10-16 12:34:19 +0300

    Configure SimpleCov for parallel tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,6 +15,10 @@ SimpleCov.start('rails') do
   add_filter ['version.rb', 'initializer.rb']
 end
 
+SimpleCov.at_exit do
+  SimpleCov.result.format!
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
@@ -26,6 +30,14 @@ class ActiveSupport::TestCase
 
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
+
+  parallelize_setup do |worker|
+    SimpleCov.command_name("test:#{worker}")
+  end
+
+  parallelize_teardown do
+    SimpleCov.result.format!
+  end
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
