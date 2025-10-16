4b024d3ee0710bffc74a903142b1fe74ab289ad9
Vladislav Kostikov <vlad@kostikov.ru>
2025-10-16 09:26:40 +0300

    Add tests for reset password email

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
+  include Rails.application.routes.url_helpers
+
   test 'task created' do
     user = create(:user)
     task = create(:task, author: user)
@@ -14,7 +16,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal ['noreply@kostikov.ru'], email.from
     assert_equal [user.email], email.to
     assert_equal 'New Task Created', email.subject
-    assert email.body.to_s.include?("Task #{task.id} was created")
+    assert email.body.to_s.include?("Task ##{task.id} – \"#{task.name}\" has been created successfully")
   end
 
   test 'task updated' do
@@ -30,7 +32,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal ['noreply@kostikov.ru'], email.from
     assert_equal [user.email], email.to
     assert_equal 'Task Updated', email.subject
-    assert email.body.to_s.include?("Task #{task.id} was updated")
+    assert email.body.to_s.include?("Task ##{task.id} – \"#{task.name}\" has been updated")
   end
 
   test 'task destroyed' do
@@ -46,6 +48,23 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal ['noreply@kostikov.ru'], email.from
     assert_equal [user.email], email.to
     assert_equal 'Task Destroyed', email.subject
-    assert email.body.to_s.include?("Task #{task.id} was destroyed")
+    assert email.body.to_s.include?("Task ##{task.id} – \"#{task.name}\" has been deleted")
+  end
+
+  test 'reset password' do
+    user = create(:user)
+    user.generate_reset_password_token!
+    params = { user: user }
+
+    email = UserMailer.with(params).reset_password_email(user)
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@kostikov.ru'], email.from
+    assert_equal [user.email], email.to
+    assert_equal 'Reset Your Password', email.subject
+    assert email.body.to_s.include?(edit_password_url(token: user.reset_password_token))
   end
 end
