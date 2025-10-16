543f8b6d559fc07b0b08e91fbda82f59095f6a17
Vladislav Kostikov <vlad@kostikov.ru>
2025-10-16 09:21:50 +0300

    Add password reset columns and methods for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,23 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :email, presence: true
   validates :first_name, :last_name, length: { minimum: 2 }
   validates :email, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/ }
+
+  def generate_reset_password_token!
+    begin
+      self.reset_password_token = SecureRandom.urlsafe_base64
+    end while User.exists?(reset_password_token: self.reset_password_token)
+
+    self.reset_password_sent_at = Time.current
+    save!
+  end
+
+  def reset_password_period_valid?
+    reset_password_sent_at && reset_password_sent_at >= 24.hours.ago
+  end
+
+  def clear_reset_password_token!
+    self.reset_password_token = nil
+    self.reset_password_sent_at = nil
+    save!
+  end
 end
---
+++ db/migrate/20251016032830_add_reset_password_to_users.rb
@@ -0,0 +1,8 @@
+class AddResetPasswordToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :reset_password_token, :string
+    add_column :users, :reset_password_sent_at, :datetime
+
+    add_index :users, :reset_password_token, unique: true
+  end
+end
